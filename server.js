ea7b220570d38c5d15e192f20bdc1eba3fcca982
Sumit Satre <sumitsatre03@gmail.com>
2025-03-23 10:44:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Route 1: Home Page
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to my simple Node.js app!');
+});
+
+// Route 2: About Page
+app.get('/about', (req, res) => {
+    res.send('This is a simple Node.js app deployed using AWS CDK.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
