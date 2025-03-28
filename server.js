f32fe74b338edb7bfda259e23560550f76e96d00
Sumit Satre <112789822+SumitSatre@users.noreply.github.com>
2025-03-28 08:39:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/about', (req, res) => {
     res.send('This is a simple Node.js app deployed using AWS CDK.');
 });
 
+app.get('/sumit', (req, res) => {
+    res.send('<h1>This is a simple Node.js app deployed using AWS CDK By Sumit Satre. Sumit Satre is a Android developer</h1>');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
